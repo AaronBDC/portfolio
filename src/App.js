99dd01c2e00c8e7d5e19a46ef0c77f0173cafd27
NETRIDER <netrider@GiR.fios-router.home>
2022-04-16 20:34:11 -0400

    parcel on the good foot

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Projects from './Projects';
+import SocialProfiles from './SocialProfiles';
+//import profile from './assets/profile.png'; // blank profile
+//import profile from './assets/profile.jpg'; // my profile 1 jpg 
+import profile from './assets/profile1.png'; // my profile 2 png 180x120 
+class App extends Component {
+    state = { displayBio: false};
+    toggleDisplayBio = () =>{
+        this.setState({ displayBio: !this.state.displayBio});
+    }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   render(){
+       return(
+        <div>
+            <img src={profile} alt='profile' className='profile' />
+            <h1>Hello!</h1>
+            <p>My name is Aaron. I'm a software engineer.</p>
+            <p>I'm always looking forward to working on meaningful projects.</p>
+            {
+                this.state.displayBio ? (
+                    <div>
+                        <p>I live in Dale City, and code every day.</p>
+                        <p>My favorite language is JavaScript, and I think Reactj.js is awesome.</p>
+                        <p>Besides coding, I also love running and barbecue!</p> 
+                        <button onClick = {this.toggleDisplayBio}>Show less</button>
+                    </div>
+                ) : (
+                    <div>
+                        <button onClick ={this.toggleDisplayBio}>Read more</button>
+                    </div>
+                )
+            }
+            <hr />
+            <Projects />
+            <hr />
+            <SocialProfiles />
+        </div>           
+       )
+   } 
 }
 
 export default App;
---
+++ src/data/projects.js
@@ -0,0 +1,60 @@
+//import project1 from '../assets/project1.png';
+//import project1 from '../assets/project1A.png';
+//import project1 from '../assets/project1B.png';
+//import project1 from '../assets/project1C.png';
+//import project1 from '../assets/project1D.png';
+import project1 from '../assets/project1E.png';
+import project2 from '../assets/project2.jpg';
+//import project2 from '../assets/project2.png';
+
+//import project3 from '../assets/project3.png';
+//import project3 from '../assets/project3A.png';
+//import project3 from '../assets/project3B.png';
+import project3 from '../assets/project3C.png';
+// import project3 from '../assets/project3D.jpg';
+//import project3 from '../assets/project3E.png';
+
+//import project4 from '../assets/project4.png';
+
+//import project5 from '../assets/project5.png';
+
+
+const PROJECTS = [
+    {
+        id: 1,
+        title: 'Discount ASCII Warehouse',
+        description: 'A React App that I built, involving JS and core web dev concepts for X-Team International!',
+        link: 'https://github.com/AaronBDC',
+        image: project1
+    },
+    {
+        id: 2,
+        title: 'PeepAPass',
+        description: 'A simple but elegant glassware that I built from scratch that was the first password list manager for Google Glass.',
+        link: 'https://github.com/AaronBDC/PeepAPass',
+        image: project2
+    },
+    {   
+        id: 3,
+        title: 'FatherShips',
+        description: 'My unique light-gun action platformer title for Nintendo WiiU console.',
+        link: 'https://rider.red/dearfans',
+        image: project3
+    }
+    // ,
+    // {   
+    //     id: 4,
+    //     title: 'BreakBooks',
+    //     description: 'My first Java based game for Nintendo WiiU Android phones and tablets.',        link: 'https://github.com/15Dkatz/example',
+    //     image: project4
+    // }
+    // ,
+    // {   
+    //     id: 5,
+    //     title: 'Marvin',
+    //     description: 'My first hand drawn game for Nintendo WiiU Android phones and tablets.',        link: 'https://github.com/15Dkatz/example',
+    //     image: project5
+    // }
+];
+
+export default PROJECTS;
